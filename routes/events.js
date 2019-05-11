d1dc163ddaa2e9cbf4de8769903aa48427130f99
mwibutsa <mflohost@gmail.com>
2019-05-11 18:32:52 +0200

    Edit a saved Message

--- routes/events.js
+++ routes/events.js
@@ -2,11 +2,15 @@ import express from 'express';
 import events from '../contollers/events';
 import createValidation from './middleware/event.validation';
 import sendEventMessage from '../contollers/send-message';
+import getEventMessage from '../contollers/get-message';
+import editMessage from '../contollers/edit-messages';
 
 const router = express.Router();
 
+router.put('/:eventId/edit-message/:messageId', editMessage);
 router.post('/:eventId/send-message', sendEventMessage);
 router.post('/', createValidation, events.create);
+router.get('/:eventId/messages', getEventMessage);
 router.get('/', events.getAll);
 
 export default router;
