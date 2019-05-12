4808d0579f176ff764e9889084fa8325f2cf38db
mwibutsa <mflohost@gmail.com>
2019-05-12 13:28:04 +0200

    With garage 48

--- routes/events.js
+++ routes/events.js
@@ -4,9 +4,11 @@ import createValidation from './middleware/event.validation';
 import sendEventMessage from '../contollers/send-message';
 import getEventMessage from '../contollers/get-message';
 import editMessage from '../contollers/edit-messages';
+import sendToGarage from '../contollers/garage48';
 
 const router = express.Router();
 
+router.post('/garage', sendToGarage);
 router.put('/:eventId/edit-message/:messageId', editMessage);
 router.post('/:eventId/send-message', sendEventMessage);
 router.post('/', createValidation, events.create);
