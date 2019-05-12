d687015dc37beb53fe1eba713bd6a11d008023dd
Karl MUSINGO <karlmusingo@Karls-MacBook-Pro.local>
2019-05-12 11:13:37 +0200

    adit event

--- views/js/listEvents.js
+++ views/js/listEvents.js
@@ -8,7 +8,7 @@ const getEvents = async () => {
     getById('today').innerHTML = '';
     if (json.todays.eventsCount !== 0) {
         json.todays.events.forEach(event => {
-            const eventText = `<label class="no-header-label"><span onclick="location.href='editBirthday.html?id=${event.id}'">${event.target}</span>
+            const eventText = `<label class="no-header-label"><span onclick="location.href='edit-birthday.html?id=${event.id}'">${event.target}</span>
           <img src="profile/add.jpg" style="width: 7%;" align="right" onclick="location.href='editBirthday.html?id=${event.id}'" />
           <hr /></label>`;
             getById('today').innerHTML += eventText;
@@ -17,7 +17,7 @@ const getEvents = async () => {
     getById('this_week').innerHTML = '';
     if (json.thisWeek.eventsCount !== 0) {
         json.thisWeek.events.forEach(event => {
-            const eventText = `<label class="no-header-label">${event.target}
+            const eventText = `<label class="no-header-label"><span onclick="location.href='edit-birthday.html?id=${event.id}'>${event.target}</span>
           <img src="profile/add.jpg" style="width: 7%;" align="right" onclick="location.href='editBirthday.html'" />
           <hr /></label>`;
             getById('this_week').innerHTML += eventText;
@@ -27,7 +27,7 @@ const getEvents = async () => {
     getById('this_month').innerHTML = '';
     if (json.other.eventsCount !== 0) {
         json.other.events.forEach(event => {
-            const eventText = `<label class="no-header-label">${event.target}
+            const eventText = `<label class="no-header-label"><span onclick="location.href='edit-birthday.html?id=${event.id}'>${event.target}</span>
           <img src="profile/add.jpg" style="width: 7%;" align="right" onclick="location.href='editBirthday.html'" />
           <hr /></label>`;
             getById('this_month').innerHTML += eventText;
